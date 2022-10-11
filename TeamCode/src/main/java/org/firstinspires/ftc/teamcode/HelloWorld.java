b6b8599d0fb1a61cef55e21caecb71b0a9892624
The1AndOnlyAlex <40921264+The1AndOnlyAlex@users.noreply.github.com>
2022-10-10 18:35:55 -0700

    Added Hello World
    Cleaned up comments as well

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -1,24 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Robot: Teleop POV", group="Robot")
-public class RobotTeleopPOV_Linear extends LinearOpMode {
-
-    /* Declare OpMode members. */
-    public DcMotor  MotorA  = null;
-    public static final double MOTOR_POWER  = 0.1;
+public class HelloWorld extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-
-        // Define and Initialize Motors
-        MotorA  = hardwareMap.get(DcMotor.class, "MotorA");
 
         // Send telemetry message to signify robot waiting;
         telemetry.addData(">", "Robot Ready.  Press Play.");    //
@@ -29,10 +19,9 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            MotorA.setPower(MOTOR_POWER);
 
             // Send telemetry message to signify robot running;
-            telemetry.addLine("Running");
+            telemetry.addLine("Hello World");
             telemetry.update();
 
             // Pace this loop so jaw action is reasonable speed.
