c3552398d32cb1585122f6db439e3036e8bc6a4f
The1AndOnlyAlex <40921264+The1AndOnlyAlex@users.noreply.github.com>
2022-10-18 18:13:02 -0700

    Added internal IMU

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleopPOV_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleopPOV_Linear.java
@@ -1,41 +1,15 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 /**
  * This particular OpMode executes a POV Game style Teleop for a direct drive robot
@@ -43,10 +17,7 @@ import com.qualcomm.robotcore.util.Range;
  *
  * In this mode the left stick moves the robot FWD and back, the Right stick turns left and right.
  * It raises and lowers the arm using the Gamepad Y and A buttons respectively.
- * It also opens and closes the claws slowly using the left and right Bumper buttons.
  *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
 @TeleOp(name="Robot: Teleop POV", group="Robot")
@@ -54,11 +25,11 @@ import com.qualcomm.robotcore.util.Range;
 public class RobotTeleopPOV_Linear extends LinearOpMode {
 
     /* Declare OpMode members. */
-    public DcMotor FrontRightMotor = null;
-    public DcMotor FrontLeftMotor  = null;
+    private DcMotor FrontRightMotor = null;
+    private DcMotor FrontLeftMotor  = null;
 
-    //Control gives the driver more control over the free speed of the motor. (min 1)
-    public static final int control = 4;
+    // Control/Expansion Hub IMU
+    private BNO055IMU imu = null;
 
     @Override
     public void runOpMode() {
@@ -68,9 +39,10 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
         double turn;
         double max;
 
-        // Define and Initialize Motors
+        // Define and Initialize Hardware
         FrontRightMotor  = hardwareMap.get(DcMotor.class, "FrontRightMotor");
         FrontLeftMotor = hardwareMap.get(DcMotor.class, "FrontLeftMotor");
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
 
         FrontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         FrontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -79,6 +51,10 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
         // leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         // rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        // define initialization values for IMU, and then initialize it.
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit            = BNO055IMU.AngleUnit.DEGREES;
+        imu.initialize(parameters);
 
         // Send telemetry message to signify robot waiting;
         telemetry.addData(">", "Robot Ready.  Press Play.");    //
@@ -90,8 +66,9 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            // Run wheels in POV mode (note: The joystick goes negative when pushed forward, so negate it)
-            // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
+            // Run wheels in POV mode (note: The joystick goes negative when pushed forward,
+            // so negate it). In this mode the Left stick moves the robot fwd and back,
+            // the Right stick turns left and right.
             // This way it's also easy to just drive straight, or just turn.
             drive = -gamepad1.left_stick_y;
             turn  =  gamepad1.right_stick_x;
@@ -108,18 +85,27 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
                 right /= max;
             }
 
-            // Output the safe vales to the motor drives. The exponent gives the driver better control over the free speed of the motor.
-
+            // Output the safe vales to the motor drives. The exponent gives the
+            // driver better control over the free speed of the motor.
             FrontRightMotor.setPower(right);
             FrontLeftMotor.setPower(left);
 
-
             telemetry.addData("left",  "%.2f", left);
             telemetry.addData("right", "%.2f", right);
+            telemetry.addData(">", "Robot Heading = %4.0f", getRawHeading());
             telemetry.update();
 
             // Pace this loop so jaw action is reasonable speed.
             sleep(50);
         }
+    }
+
+    /**
+     * read the raw (un-offset Gyro heading) directly from the IMU
+     */
+    private double getRawHeading() {
+        Orientation angles   = imu.getAngularOrientation(AxesReference.INTRINSIC,
+                AxesOrder.ZYX, AngleUnit.DEGREES);
+        return angles.firstAngle;
     }
 }
