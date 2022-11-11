68f8090e3ec960e2e3a47248318337a53a10a592
JadonLong12 <201212459@lbschools.net>
2022-11-11 11:50:35 -0800

    Create FinalControlScheme.java
    This is the theoretical final control scheme should I have coded this perfectly on the first try without testing it at all.
    
    The linear slide motor should be named "MotorC"
    
    The claw servo should be named "ServoA"

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalControlScheme.java
@@ -0,0 +1,153 @@
+/*
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * This particular OpMode executes a POV Game style Teleop for a direct drive robot
+ * The code is structured as a LinearOpMode
+ *
+ * In this mode the left stick moves the robot FWD and back, the Right stick turns left and right.
+ * It raises and lowers the arm using the Gamepad Y and A buttons respectively.
+ * It also opens and closes the claws slowly using the left and right Bumper buttons.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+@TeleOp(name="Robot: Teleop POV", group="Robot")
+public class FinalControlScheme extends LinearOpMode {
+    /* Declare OpMode members. */
+    public DcMotor  leftDrive   = null;
+    public DcMotor  rightDrive  = null;
+    public DcMotor  linearSlide = null;
+    public Servo claw = null;
+
+    public static final double MIN_POSITION  =  0.0 ;
+    public static final double MAX_POSITION  =  0.5 ;
+
+    @Override
+    public void runOpMode() {
+        double left;
+        double right;
+        double drive;
+        double turn;
+        boolean lock;
+        double speedMult;
+        ////SLOW CONTROL (min 1) gives the driver better control over slower speeds, but worse control over faster speeds.
+
+        // Define and Initialize Motors and Servos
+        leftDrive  = hardwareMap.get(DcMotor.class, "MotorA");
+        rightDrive = hardwareMap.get(DcMotor.class, "MotorB");
+        linearSlide = hardwareMap.get(DcMotor.class, "MotorC");
+        claw = hardwareMap.get(Servo.class, "ServoA");
+        //leftArm    = hardwareMap.get(DcMotor.class, "left_arm");
+        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
+        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        linearSlide.setDirection(DcMotor.Direction.FORWARD);
+        // If there are encoders connected, switch to RUN_USING_ENCODER mode for greater accuracy
+        // leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        claw.setPosition(0.0);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData(">", "Robot Ready.  Press Play.");    //
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            // Run wheels in POV mode (note: The joystick goes negative when pushed forward, so negate it)
+            // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
+            // This way it's also easy to just drive straight, or just turn.
+            drive = -1 * gamepad1.left_stick_y;
+            turn  =  gamepad1.right_stick_x;
+            lock = gamepad1.right_bumper;
+
+            //Handle speed multiplication
+            if (gamepad1.left_bumper){
+                speedMult = 1;
+                telemetry.addData("Speed: ", "String", "Fast");
+            }
+            else if (gamepad1.right_bumper){
+                speedMult = 0.25;
+                telemetry.addData("Speed: ", "String", "Slow");
+            }
+            else {
+                speedMult = 0.5;
+                telemetry.addData("Speed: ", "String""Normal");
+            }
+
+            //Handle claw open and close
+            if (gamepad1.x){
+                claw.setPosition(0);
+            }
+            else if (gamepad1.a){
+                claw.setPosition(1);
+            }
+
+            //Handle linear slide movement
+            if (gamepad1.left_trigger >= 0.5){
+                linearSlide.setPower(1);
+            }
+            else if (gamepad1.right_trigger >= 0.5){
+                linearSlide.setPower(-1);
+            }
+
+            //Drive!
+            // Combine drive and turn for blended motion.
+            left = drive + turn;
+            right = drive - turn;
+            // Normalize the values so neither exceed +/- 1.0
+            if (left > 1.0) {
+                left = 1.0;
+            }
+            if (right > 1.0){
+                right = 1.0;
+            }
+            // Output the safe vales to the motor drives.
+            leftDrive.setPower(Math.pow(left, 3) * speedMult);
+            rightDrive.setPower(Math.pow(right, 3) * speedMult);
+
+            // Send telemetry message to signify robot running;
+            //telemetry.addData("claw",  "Offset = %.2f", clawOffset);
+            telemetry.addData("X: ",  "%.2f", turn);
+            telemetry.addData("Y: ", "%.2f", (drive * -1));
+            if (lock) {telemetry.addData("Lock: ", "%.2f", "ON!");}
+            else {telemetry.addData("Lock: ", "%.2f", "OFF!");}
+            telemetry.update();
+            // Pace this loop so jaw action is reasonable speed.
+            sleep(50);
+        }
+    }
+}
