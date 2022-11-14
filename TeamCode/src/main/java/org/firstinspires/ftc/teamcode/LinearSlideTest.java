3f2d6e478a99fca739b54731e721c3762fedcd84
osun24 <114966159+osun24@users.noreply.github.com>
2022-11-13 19:14:52 -0800

    Prototype for linear slide macro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideTest.java
@@ -3,29 +3,26 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
 
 @TeleOp(name="LinearSlideTest", group="Robot")
 public class LinearSlideTest extends LinearOpMode {
     /* Declare OpMode members. */
-    public DcMotor leftDrive   = null;
-    public DcMotor  rightDrive  = null;
+    public DcMotor linearSlide = null;
 
     @Override
     public void runOpMode() {
-        double left;
+        double slideLocation = 0; //tracks the linear slide's location based on how many times the motor has ran
 
         // Define and Initialize Motors
-        leftDrive  = hardwareMap.get(DcMotor.class, "MotorA");
+        linearSlide = hardwareMap.get(DcMotor.class, "MotorC");
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        linearSlide.setDirection(DcMotor.Direction.REVERSE);
 
         // Send telemetry message to signify robot waiting;
-        telemetry.addData(">", "Robot Ready.  Press Play.");    //
+        telemetry.addData(">", "Robot Ready.  Press Play.");
         telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
@@ -33,19 +30,29 @@ public class LinearSlideTest extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            telemetry.addData("Hello", "Motor test in progress.");
+            telemetry.addData("Hello", "Linear Slide test in progress.");
 
-            // Normalize the values so neither exceed +/- 1.0
-            left = gamepad1.right_trigger;
-
-            if (left > 1.0) {
-                left /= 1.0;
+            if (gamepad1.x) {
+                if (slideLocation == 0){ //0 represents standstill
+                    slideLocation += 1; //get out of standstill to move up
+                } else {
+                    slideLocation = -slideLocation; //reverse direction if x button is clicked
+                }
             }
 
-            // Output the safe vales to the motor drives.
-            leftDrive.setPower(left);
-
-            telemetry.addData("left",  "%.2f", left);
+            if (slideLocation > 0) { //going up
+                if (Math.abs(slideLocation) <= 200) { //currently requires a little more than 10 seconds, or 10,000 ms to reach top; based on 50 ms loops
+                    linearSlide.setPower(1.0);
+                    slideLocation += 1;
+                    telemetry.addData("Linear Slide Direction: ", "Up");
+                }
+            } else { //going down
+                if (slideLocation < 0) {
+                    linearSlide.setPower(-1.0);
+                    slideLocation += 1;
+                    telemetry.addData("Linear Slide Direction: ", "Down");
+                }
+            }
 
             telemetry.update();
 
