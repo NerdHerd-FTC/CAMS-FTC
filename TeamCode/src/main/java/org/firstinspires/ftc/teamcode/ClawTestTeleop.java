811d11eb9c53970b1a9178d336e3454ad683764e
The1AndOnlyAlex <40921264+The1AndOnlyAlex@users.noreply.github.com>
2022-12-04 08:15:56 -0800

    ClawValues

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawTestTeleop.java
@@ -0,0 +1,110 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+/**
+ * AD 12/2/22
+ */
+
+@TeleOp(name="Robot: Claw Teleop", group="Robot")
+//@Disabled
+public class ClawTestTeleop extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    public Servo finger;
+    public Servo palm;
+    public Servo wrist;
+
+    @Override
+    public void runOpMode() {
+
+        // Define and Initialize Hardware
+        finger = hardwareMap.get(Servo.class, "finger");
+        palm = hardwareMap.get(Servo.class, "palm");
+        wrist = hardwareMap.get(Servo.class, "wrist");
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData(">", "Robot Ready.  Press Play.");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        double fingerTargetPos = 0.7;
+        double palmTargetPos = 0.2;
+        double wristTargetPos = 0.3;
+
+        while (opModeIsActive()) {
+
+            // finger
+            if(gamepad2.left_stick_x > 0) {
+                fingerTargetPos += 0.05;
+            }
+            else if(gamepad2.left_stick_x < 0) {
+                fingerTargetPos -= 0.05;
+            }
+
+            if(fingerTargetPos >= 1.0) {
+                fingerTargetPos = 1.0;
+            }
+            else if(fingerTargetPos <= 0.0) {
+                fingerTargetPos = 0.0;
+            }
+
+            // palm
+            if(gamepad2.right_stick_x > 0) {
+                palmTargetPos += 0.05;
+            }
+            else if(gamepad2.right_stick_x < 0) {
+                palmTargetPos -= 0.05;
+            }
+
+            if(palmTargetPos >= 1.0) {
+                palmTargetPos = 1.0;
+            }
+            else if(palmTargetPos <= 0.0) {
+                palmTargetPos = 0.0;
+            }
+
+            // wrist
+            if(gamepad2.dpad_up) {
+                wristTargetPos += 0.05;
+            }
+            else if(gamepad2.dpad_down) {
+                wristTargetPos -= 0.05;
+            }
+
+            if(wristTargetPos >= 1.0) {
+                wristTargetPos = 1.0;
+            }
+            else if(wristTargetPos <= 0.0) {
+                wristTargetPos = 0.0;
+            }
+
+
+            finger.setPosition(fingerTargetPos);
+            palm.setPosition(palmTargetPos);
+            wrist.setPosition(wristTargetPos);
+            telemetry.addData("fingerTargetPos", "%.2f", fingerTargetPos);
+            telemetry.addData("palmTargetPos", "%.2f", palmTargetPos);
+            telemetry.addData("wristTargetPos", "%.2f", wristTargetPos);
+            telemetry.update();
+
+
+            // Pace this loop so jaw action is reasonable speed.
+            sleep(50);
+        }
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleopPOV_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleopPOV_Linear.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
