0fdbe4f10572e2734dbc7ff4c619bb58f197fe97
osun24 <114966159+osun24@users.noreply.github.com>
2022-12-24 22:37:26 -0800

    add pid w/ imu turning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PDwithIMU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PDwithIMU.java
@@ -21,17 +21,27 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 @Autonomous(name="PD with IMU", group="Robot")
 public class PDwithIMU extends LinearOpMode
 {
     public DcMotor leftDrive = null;
     public DcMotor rightDrive = null;
+
+    static RevHubOrientationOnRobot.LogoFacingDirection[] logoFacingDirections
+            = RevHubOrientationOnRobot.LogoFacingDirection.values();
+    static RevHubOrientationOnRobot.UsbFacingDirection[] usbFacingDirections
+            = RevHubOrientationOnRobot.UsbFacingDirection.values();
+    IMU imu;
 
     static final double     COUNTS_PER_MOTOR_REV    = 28 ;    //UltraPlanetary Gearbox Kit & HD Hex Motor
     static final double     DRIVE_GEAR_REDUCTION    = 20;   //gear ratio
@@ -61,23 +71,24 @@ public class PDwithIMU extends LinearOpMode
 
         telemetry.setMsTransmissionInterval(50);
 
+        //initialize & setup IMU
+        imu = hardwareMap.get(IMU.class, "imu");
+        RevHubOrientationOnRobot.LogoFacingDirection logo = logoFacingDirections[0]; //logo facing UP
+        RevHubOrientationOnRobot.UsbFacingDirection usb = usbFacingDirections[4]; //usb ports facing to the LEFT
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logo, usb);
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+
         waitForStart();
         // run until the end of the match (driver presses STOP)
-        forward(-48);
-        forward(48);
-        forward(-48);
-        forward(48);
-        forward(-48);
-        forward(48);
-        forward(-48);
-        forward(48);
-        forward(-48);
-        forward(48);
-        forward(-48);
-        forward(48);
+        //forwardPID(-48);
+        //forwardPID(48);
+        //builtIn(-48, -48);
+        //builtIn(-48, -48);
+        turnIMU(90);
+        turnIMU(-90);
     }
 
-    private void forward(double targetInches) {
+    private void forwardPID(double targetInches) {
         int location = leftDrive.getCurrentPosition();
         final int target = (int) (COUNTS_PER_INCH * targetInches) + location; //in encoder ticks
         double error = (target - location);
@@ -101,6 +112,7 @@ public class PDwithIMU extends LinearOpMode
             leftDrive.setPower(finalPower);
             rightDrive.setPower(finalPower);
 
+            telemetry.addLine("CUSTOM");
             telemetry.addData("Location: ", leftDrive.getCurrentPosition());
             telemetry.addData("Target: ", target);
             telemetry.addData("Error Number: ", error);
@@ -116,6 +128,61 @@ public class PDwithIMU extends LinearOpMode
             }
 
             sleep(DELTA_T);
+        }
+    }
+    private void builtIn(double leftInches, double rightInches) {
+        int newLeftTarget = leftDrive.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+        int newRightTarget = rightDrive.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+
+        if (opModeIsActive()) {
+            leftDrive.setTargetPosition(newLeftTarget);
+            rightDrive.setTargetPosition(newRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+        while (opModeIsActive() && leftDrive.isBusy() && rightDrive.isBusy()) {
+            YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles(); //get yaw from imu, helps to measure drift
+            AngularVelocity angularVelocity = imu.getRobotAngularVelocity(AngleUnit.DEGREES);
+
+            telemetry.addLine("BUILT IN");
+            telemetry.addData("Running to", newLeftTarget);
+            telemetry.addData("Currently at", leftDrive.getCurrentPosition());
+            telemetry.addData("Yaw", "%.2f Deg. (Heading)", orientation.getYaw(AngleUnit.DEGREES));
+            telemetry.addData("Yaw Velocity", "%.2f Deg/Sec", angularVelocity.zRotationRate); //rotational location
+            telemetry.update();
+        }
+    }
+
+    private void turnIMU (double degrees) {
+        //K constants
+        final double K_P_MOVE = 0.0004;
+        final double K_D_MOVE = 0;
+
+        while (opModeIsActive() && leftDrive.isBusy() && rightDrive.isBusy()) {
+            YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+            double currentAngle = orientation.getYaw(AngleUnit.DEGREES);
+
+            double error = degrees - currentAngle;
+
+            //get most efficient angle (imu has angles from -180 to 180)
+            if (error > 180) {
+                error -= 360;
+            } else if (error < -180) {
+                error += 360;
+            }
+
+            //set power: find power value and restrict it to +- 1 and then flip it because our motors are weird
+            double power = Math.tanh(K_P_MOVE * error * 5.968915978362711); //convert angle to ticks so that the P still applies
+            leftDrive.setPower(-power);
+            rightDrive.setPower(power);
+
+            telemetry.addLine("ROTATING");
+            telemetry.addData("Target", degrees);
+            telemetry.addData("Error", error);
+            telemetry.addData("Yaw", "%.2f Deg. (Heading)", orientation.getYaw(AngleUnit.DEGREES));
+            telemetry.update();
         }
     }
 }
